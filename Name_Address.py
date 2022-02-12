a0e7fd54b2fe36dade7d2eeb27ba7688513e8276
mkabir88 <95720420+mkabir88@users.noreply.github.com>
2022-02-13 00:35:34 +0200

    Initial commit

---
+++ Name_Address.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# A program which asks for the user's name and address and print out the given information.
+
+# In[ ]:
+
+
+given_name= input("Given name: ")
+family_name= input("Family name: ")
+street_address= input("Street address: ")
+city_and_postal_code= input("City and Postal code: ")
+print(given_name +" "+ family_name)
+print(street_address)
+print(city_and_postal_code)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
